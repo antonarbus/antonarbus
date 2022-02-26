c556f0a8407f4ac57c2469f39c8b7add485009fd
Anton Windows Home <sherbsherb@gmail.com>
2022-02-26 21:13:15 +0200

    git lense

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
+          I am sherb
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
