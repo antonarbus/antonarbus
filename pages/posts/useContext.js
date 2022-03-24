481582047cdc452c278fcca74e9f247554eca7bd
Anton Windows Home <sherbsherb@gmail.com>
2022-03-24 16:54:58 +0200

    useContext

--- pages/posts/useContext.js
+++ pages/posts/useContext.js
@@ -1,15 +1,18 @@
-import { Code, H3, CodeSpan } from '/components/post/reExport';
-import { createContext, useContext } from "react"
+import { Code, H3, CodeSpan } from '/components/post/reExport'
+import { createContext, useContext, useState, useReducer } from "react"
 import { OnePost } from '/components/post/OnePost'
-
-const style = {border: "1px solid grey", padding: "10px", margin: "10px"}
-const Context = createContext()
+import shortid from 'shortid'
 
 export default function index() {
   return <OnePost post={postObj} />;
 }
 
-function Component() {
+// #region useContext()
+
+const style = {border: "1px solid grey", padding: "10px", margin: "10px"}
+const Context = createContext()
+
+function Parent1() {
   return (
     <Context.Provider value={{age: 37}}>
       <A name='John' />
@@ -51,7 +54,7 @@ function D(props) {
     </div>
   )
 }
-function Age(props) {
+function Age() {
   const context = useContext(Context)
   return (
     <div style={style}>
@@ -60,6 +63,91 @@ function Age(props) {
   )
 }
 
+// #endregion
+
+// #region useContext() & state
+
+const Context2 = createContext('');
+
+function Parent2() {
+  const [string, setString] = useState(shortid())
+
+  const contextValue={
+    string,
+    setString,
+    style: { border: '2px solid grey',  padding: '10px',  margin: '10px' }
+  }
+
+  return (
+    <Context2.Provider value={contextValue}>
+      <div style={contextValue.style}>
+        <div>Parent</div>
+        <div>String from context: <b>{contextValue.string}</b></div>
+        <button onClick={() => contextValue.setString(shortid()) }>Change string</button>
+        <Child2 name='Child 1'/>
+        <Child2 name='Child 2'/>
+      </div>
+    </Context2.Provider>
+  )
+}
+
+function Child2(props) {
+  const contextValue = useContext(Context2);
+  return (
+    <div style={contextValue.style}>
+      <div>{props.name}</div>
+      <div>String from context: <b>{contextValue.string}</b></div>
+      <button onClick={() => contextValue.setString(shortid()) }>Change string</button>
+    </div>
+  )
+}
+
+// #endregion
+
+// #region same with useReducer()
+
+const Context3 = createContext('');
+
+function reducer(state, action) {
+  if (action === 'randomize string') return shortid()
+  return state
+}
+
+function Parent3() {
+  const [string, dispatch] = useReducer(reducer, shortid())
+
+  const contextValue={
+    string,
+    dispatch,
+    style: { border: '2px solid grey',  padding: '10px',  margin: '10px' }
+  }
+
+  return (
+    <Context3.Provider value={contextValue}>
+      <div style={contextValue.style}>
+        <div>Parent</div>
+        <div>String from context: <b>{contextValue.string}</b></div>
+        <button onClick={() => contextValue.dispatch('randomize string') }>Change string</button>
+        <Child3 name='Child 1'/>
+        <Child3 name='Child 2'/>
+      </div>
+    </Context3.Provider>
+  )
+}
+
+function Child3(props) {
+  const contextValue = useContext(Context3);
+  return (
+    <div style={contextValue.style}>
+      <div>{props.name}</div>
+      <div>String from context: <b>{contextValue.string}</b></div>
+      <button onClick={() => contextValue.dispatch('randomize string') }>Change string</button>
+    </div>
+  )
+}
+
+// #endregion
+
 export const postObj = {
   title: 'useContext',
   date: '2021.09.26',
@@ -67,8 +155,7 @@ export const postObj = {
   description: 'useContext react hook',
   body: (
     <>
-
-      <H3>Pass props vs useContext</H3>
+      <H3>Pass props vs <CodeSpan>useContext()</CodeSpan></H3>
 
       <ul>
         <li>In a project we may have many components and use one inside another ones</li>
@@ -83,12 +170,13 @@ export const postObj = {
       </ul>
 
       <Code>{`
-      import { createContext, useContext } from "react"
+      import { createContext, useContext, useState, useReducer } from "react"
+      import shortid from 'shortid'
 
       const style = {border: "1px solid grey", padding: "10px", margin: "10px"}
       const Context = createContext()
 
-      function Component() {
+      function Parent1() {
         return (
           <Context.Provider value={{age: 37}}>
             <A name='John' />
@@ -130,7 +218,7 @@ export const postObj = {
           </div>
         )
       }
-      function Age(props) {
+      function Age() {
         const context = useContext(Context)
         return (
           <div style={style}>
@@ -140,7 +228,104 @@ export const postObj = {
       }
       `}</Code>
 
-      <Component />
+      <Parent1 />
+
+      <H3>Pass state in <CodeSpan>useContext()</CodeSpan></H3>
+
+      <p>Same idea as above, but pass a state variable into <CodeSpan>{'<Context.Provider value={state}>'}</CodeSpan>  </p>
+
+      <Code>{`
+        import { createContext, useContext, useState, useReducer } from "react"
+        import shortid from 'shortid'
+
+        const Context2 = createContext('');
+
+        function Parent2() {
+          const [string, setString] = useState(shortid())
+
+          const contextValue={
+            string,
+            setString,
+            style: { border: '2px solid grey',  padding: '10px',  margin: '10px' }
+          }
+
+          return (
+            <Context2.Provider value={contextValue}>
+              <div style={contextValue.style}>
+                <div>Parent</div>
+                <div>String from context: <b>{contextValue.string}</b></div>
+                <button onClick={() => contextValue.setString(shortid()) }>Change string</button>
+                <Child2 name='Child 1'/>
+                <Child2 name='Child 2'/>
+              </div>
+            </Context2.Provider>
+          )
+        }
+
+        function Child2(props) {
+          const contextValue = useContext(Context2);
+          return (
+            <div style={contextValue.style}>
+              <div>{props.name}</div>
+              <div>String from context: <b>{contextValue.string}</b></div>
+              <button onClick={() => contextValue.setString(shortid()) }>Change string</button>
+            </div>
+          )
+        }
+      `}</Code>
+
+      <Parent2 />
+
+      <H3><CodeSpan>useContext()</CodeSpan> with <CodeSpan>useReducer()</CodeSpan></H3>
+
+      <p>Same as above, but instead of <CodeSpan>useState()</CodeSpan> hook <CodeSpan>useReducer()</CodeSpan> is used.</p>
+
+      <Code>{`
+        import { createContext, useContext, useState, useReducer } from "react"
+        import shortid from 'shortid'
+
+        const Context3 = createContext('');
+
+        function reducer(state, action) {
+          if (action === 'randomize string') return shortid()
+          return state
+        }
+
+        function Parent3() {
+          const [string, dispatch] = useReducer(reducer, shortid())
+
+          const contextValue={
+            string,
+            dispatch,
+            style: { border: '2px solid grey',  padding: '10px',  margin: '10px' }
+          }
+
+          return (
+            <Context3.Provider value={contextValue}>
+              <div style={contextValue.style}>
+                <div>Parent</div>
+                <div>String from context: <b>{contextValue.string}</b></div>
+                <button onClick={() => contextValue.dispatch('randomize string') }>Change string</button>
+                <Child3 name='Child 1'/>
+                <Child3 name='Child 2'/>
+              </div>
+            </Context3.Provider>
+          )
+        }
+
+        function Child3(props) {
+          const contextValue = useContext(Context3);
+          return (
+            <div style={contextValue.style}>
+              <div>{props.name}</div>
+              <div>String from context: <b>{contextValue.string}</b></div>
+              <button onClick={() => contextValue.dispatch('randomize string') }>Change string</button>
+            </div>
+          )
+        }
+      `}</Code>
+
+      <Parent3 />
     </>
   ),
 }
